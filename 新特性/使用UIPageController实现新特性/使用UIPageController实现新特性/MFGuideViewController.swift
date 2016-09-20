6e2d912685e06d26c7e5c3f2a4135129a1cce479
Michael <flcyfr@163.com>
2016-09-20 23:49:10 +0800

    add pageController app new feature

---
+++ 新特性/使用UIPageController实现新特性/使用UIPageController实现新特性/MFGuideViewController.swift
@@ -0,0 +1,64 @@
+//
+//  MFGuideViewController.swift
+//  使用UIPageController实现新特性
+//
+//  Created by flc on 16/9/20.
+//  Copyright © 2016年 flc. All rights reserved.
+//
+
+import UIKit
+
+class MFGuideViewController: UIViewController {
+    
+    private(set) lazy var pageControl: UIPageControl = {
+        let rect = CGRect(x: (self.view.bounds.width - 200) / 2, y: self.view.bounds.height - 20 - 10, width: 200, height: 20)
+        let pageControl = UIPageControl(frame: rect)
+        pageControl.pageIndicatorTintColor = UIColor.white
+        pageControl.currentPageIndicatorTintColor = UIColor.orange
+        return pageControl
+    }()
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        NotificationCenter.default.addObserver(self, selector: #selector(removeGuidePageView), name: NSNotification.Name(rawValue: "beginExperience"), object: nil)
+        view.backgroundColor = UIColor.white
+        view.addSubview(pageControl)
+        loadGuidePageView()
+        
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    dynamic private func removeGuidePageView() {
+        view.removeFromSuperview()
+        self.removeFromParentViewController()
+        
+    }
+    
+    private func loadGuidePageView() {
+        let guideViewController = MFGuidePageViewController(transitionStyle: .pageCurl, navigationOrientation: .horizontal, options: nil, pageCount: { (guide:MFGuidePageViewController, count: NSInteger) in
+            self.pageControl.numberOfPages = count
+        }) { (guide: MFGuidePageViewController, index: NSInteger) in
+                self.pageControl.currentPage = index
+        }
+        
+        guideViewController.view.frame = view.bounds
+        view.addSubview(guideViewController.view)
+        addChildViewController(guideViewController)
+        view.bringSubview(toFront: pageControl)
+        
+    }
+    
+}
