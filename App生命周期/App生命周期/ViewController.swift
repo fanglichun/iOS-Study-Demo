cdf32b86b53de6cacd2c484165d4fd5e931a1a03
Michael <flcyfr@163.com>
2016-10-04 13:38:12 +0800

    add a demo about app life style

---
+++ App生命周期/App生命周期/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  App生命周期
+//
+//  Created by flc on 16/10/1.
+//  Copyright © 2016年 Michael. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
