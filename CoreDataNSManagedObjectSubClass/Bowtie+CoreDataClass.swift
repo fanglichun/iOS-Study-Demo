0b9ec4f51c2ef493e813ac68ddec96a758b27d73
Michael <flcyfr@163.com>
2016-10-08 18:54:06 +0800

    no msg

---
+++ CoreDataNSManagedObjectSubClass/Bowtie+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Bowtie+CoreDataClass.swift
+//  CoreDataNSManagedObjectSubClass
+//
+//  Created by flc on 16/10/5.
+//  Copyright © 2016年 Michael. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+public class Bowtie: NSManagedObject {
+
+}
