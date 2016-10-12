716e9629d967302157846ee5422dab56fb10c41b
Michael <flcyfr@163.com>
2016-10-13 00:29:48 +0800

    add a demo about swift3.0 gcd;

--- Queue/Queue/ViewController.swift
+++ Queue/Queue/ViewController.swift
@@ -18,7 +18,10 @@ class ViewController: UIViewController {
         //globalQueueSync()
         //customizeDefalutQueueAsync()
         //customizeDefalutQueueSync()
-        customizeQueueAsync()
+        //customizeQueueAsync()
+        //delayQueue()
+        //groupQueue()
+        barrierAsync()
     }
     
     override func didReceiveMemoryWarning() {
@@ -99,7 +102,7 @@ fileprivate extension ViewController {
     func customizeQueueAsync() {
         print("Before: \(Thread.current)")
         let queue = DispatchQueue(label: "", qos: .background, attributes: DispatchQueue.Attributes.concurrent, autoreleaseFrequency: DispatchQueue.AutoreleaseFrequency.workItem, target: nil)
-//        let queue = DispatchQueue(label: "queueAsync", qos: DispatchQoS.init(qosClass: DispatchQoS.QoSClass.background, relativePriority: 0), attributes: DispatchQueue.Attributes.concurrent, autoreleaseFrequency: DispatchQueue.AutoreleaseFrequency.workItem, target: nil)
+        //        let queue = DispatchQueue(label: "queueAsync", qos: DispatchQoS.init(qosClass: DispatchQoS.QoSClass.background, relativePriority: 0), attributes: DispatchQueue.Attributes.concurrent, autoreleaseFrequency: DispatchQueue.AutoreleaseFrequency.workItem, target: nil)
         
         for i in 0...10 {
             print(i)//time-consuming operation;
@@ -111,5 +114,112 @@ fileprivate extension ViewController {
         print("After: \(Thread.current)")
         
     }
+    
+    func customizeGlobal() {
+        print("Before: \(Thread.current)")
+        let s = DispatchQueue.global(qos: DispatchQoS.QoSClass.default) //userInitiated/background   utility/userInteractive/unspecified/default
+        for i in 0...10 {
+            print("current Thread \(i): \(Thread.current)")
+            s.async(execute: {
+                print("current Thread: \(Thread.current)")
+                
+            })
+        }
+        print("After: \(Thread.current)")
+    }
+    
+    func delayQueue() {
+        let queue = DispatchQueue.global(qos: .default)
+        queue.asyncAfter(wallDeadline: DispatchWallTime.now() + 10) {
+            print("10s 后执行")
+        }
+    }
+    /*
+     DISPATCH_QUEUE_PRIORITY_HIGH:        .userInitiated
+     DISPATCH_QUEUE_PRIORITY_DEFAULT:      .default
+     DISPATCH_QUEUE_PRIORITY_LOW:          .utility
+     DISPATCH_QUEUE_PRIORITY_BACKGROUND:  .background
+     */
+    func groupQueue() {
+        let globalQueue = DispatchQueue.global(qos: DispatchQoS.QoSClass.userInitiated)
+        let group = DispatchGroup()
+        group.enter()
+        globalQueue.async(group: group, qos: .userInitiated, flags: DispatchWorkItemFlags.barrier) {
+            print("1")
+            
+        }
+        globalQueue.async(group: group, qos: .userInitiated, flags: DispatchWorkItemFlags.barrier) {
+            print("2")
+            
+        }
+        globalQueue.async(group: group, qos: .userInitiated, flags: DispatchWorkItemFlags.barrier) {
+            print("3")
+            
+        }
+        group.notify(qos: .userInitiated, flags: .barrier, queue: globalQueue) {
+            print("completed")
+            group.leave()
+        }
+//        group.wait(wallTimeout: .now() + 5)
+//        group.wait(timeout: DispatchTime.now() + 5)
+//        print("completed")
+
+    }
+    
+    func barrierAsync() {
+        writeFile(5)
+        let globalQueue = DispatchQueue.global(qos: DispatchQoS.QoSClass.default)
+        let group = DispatchGroup()
+        globalQueue.async { [weak self] in
+            self?.readFile()
+        }
+        
+        globalQueue.async { [weak self] in
+            self?.readFile()
+        }
+        
+        globalQueue.async { [weak self] in
+            self?.readFile()
+        }
+        globalQueue.async { [weak self] in
+            self?.readFile()
+        }
+        globalQueue.async(group: group, qos: DispatchQoS.userInitiated, flags: DispatchWorkItemFlags.barrier) { [weak self] in
+            self?.writeFile(9)
+            self?.readFile()
+        }
+        
+        globalQueue.async { [weak self] in
+            self?.readFile()
+        }
+        
+        globalQueue.async { [weak self] in
+            self?.readFile()
+        }
+        globalQueue.async { [weak self] in
+            self?.readFile()
+        }
+    }
+    
+    func applyAsync() {
+//        let globalQueue = DispatchQueue.global(qos: DispatchQoS.QoSClass.default)
+//        let array = ["sb", "ssdbsd", "liCHun", "Fang", "Michael"]
+//        globalQueue.async(execute: { () -> Void in
+//            globalQueue.apply(attributes: 10, iterations: globalQueue) { (index) -> Void in
+//                print(index)
+//            }
+//            print("completed")
+//        })
+        
+    }
+    
+    private func writeFile(_ num: Int) {
+        UserDefaults.standard.set(num, forKey: "Integer_Key")
+    }
+    
+    private func readFile() {
+        print(UserDefaults.standard.integer(forKey: "Integer_Key"))
+    }
+    
 }
 
