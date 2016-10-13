0b54640c96501b851c297c20d06e3ce070b0448c
mfang032 <michal.fang@pwc.com>
2016-10-13 17:23:27 +0800

    add gcd demo

--- Queue/Queue/ViewController.swift
+++ Queue/Queue/ViewController.swift
@@ -13,15 +13,53 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        //mainQueueAsync()
-        //globalQueueAsync()
-        //globalQueueSync()
-        //customizeDefalutQueueAsync()
-        //customizeDefalutQueueSync()
-        //customizeQueueAsync()
-        //delayQueue()
-        //groupQueue()
-        barrierAsync()
+//        mainQueueAsync()
+//        globalQueueAsync()
+//        globalQueueSync()
+//        customizeDefalutQueueAsync()
+//        customizeDefalutQueueSync()
+//        customizeQueueAsync()
+//        delayQueue()
+//        groupConcurrentQueue()
+//        for _ in 0...5 {
+//            groupSerialQueue()
+//        }
+        //barrierAsync()
+//        let globalQueue = DispatchQueue.global(qos: DispatchQoS.QoSClass.userInitiated)
+//        let serialQueue = DispatchQueue(label: "SerialQueue")
+//        let concurrentQueue = DispatchQueue(label: "ConcurrentQueue", qos: DispatchQoS.userInitiated, attributes: DispatchQueue.Attributes.concurrent, autoreleaseFrequency: DispatchQueue.AutoreleaseFrequency.workItem, target: nil)
+//        let delayTime = DispatchTime.now() + DispatchTimeInterval.seconds(10)
+//        DispatchQueue.main.asyncAfter(deadline: delayTime, qos: DispatchQoS.userInitiated, flags: DispatchWorkItemFlags.noQoS) {
+//            print("delay \(delayTime)s后执行")
+//        }
+//        let group = DispatchGroup()
+        /*
+        let myQueue = DispatchQueue(label: "my.queue", attributes: .concurrent)
+        let workItem = DispatchWorkItem {
+            print("done")
+        }
+        myQueue.async(execute: workItem)
+        print("before waiting")
+        workItem.wait()
+        print("after waiting")
+        */
+        /*
+        let wirte = DispatchWorkItem(flags: .barrier) {
+            // write data
+            print("write data ")
+        }
+        let dataQueue = DispatchQueue(label: "data", attributes: .concurrent)
+            dataQueue.async(execute: wirte)*/
+        
+        /*
+        let writeWorkItem = DispatchWorkItem(qos: DispatchQoS.default, flags: DispatchWorkItemFlags.barrier) {
+            print("write data ")
+        }
+        let dataQueue = DispatchQueue(label: "write data Queue", qos: DispatchQoS.default, attributes: DispatchQueue.Attributes.concurrent, autoreleaseFrequency: DispatchQueue.AutoreleaseFrequency.inherit, target: nil)
+        dataQueue.async(execute: writeWorkItem)*/
+        
+        semaphore()
+        
     }
     
     override func didReceiveMemoryWarning() {
@@ -140,7 +178,7 @@ fileprivate extension ViewController {
      DISPATCH_QUEUE_PRIORITY_LOW:          .utility
      DISPATCH_QUEUE_PRIORITY_BACKGROUND:  .background
      */
-    func groupQueue() {
+    func groupConcurrentQueue() {
         let globalQueue = DispatchQueue.global(qos: DispatchQoS.QoSClass.userInitiated)
         let group = DispatchGroup()
         group.enter()
@@ -163,7 +201,25 @@ fileprivate extension ViewController {
 //        group.wait(wallTimeout: .now() + 5)
 //        group.wait(timeout: DispatchTime.now() + 5)
 //        print("completed")
-
+    }
+    
+    func groupSerialQueue() {
+       let workIyem = DispatchWorkItem(qos: DispatchQoS.userInitiated, flags: DispatchWorkItemFlags.assignCurrentContext) {
+        //assignCurrentContext表示QoS根据创建时的context决定
+        }
+        let group = DispatchGroup()
+        let bookQueue = DispatchQueue(label: "download Book Queue")
+        bookQueue.async(group: group, qos: DispatchQoS.userInitiated, flags: DispatchWorkItemFlags.detached) {
+            print("download book")
+        }
+        let videoQueue = DispatchQueue(label: "download Video queue")
+        videoQueue.async(group: group, qos: DispatchQoS.userInitiated, flags: DispatchWorkItemFlags.detached) {
+            print("download video")
+        }
+        group.wait()
+        group.notify(queue: DispatchQueue.main) {
+            print("download completed,refresh UI")
+        }
     }
     
     func barrierAsync() {
@@ -219,6 +275,17 @@ fileprivate extension ViewController {
     
     private func readFile() {
         print(UserDefaults.standard.integer(forKey: "Integer_Key"))
+    }
+    
+    func semaphore() {//The latest time to wait for a signal.
+        let semaphore = DispatchSemaphore(value: 5)
+        semaphore.signal()
+        print(semaphore)
+        let delay = semaphore.wait(wallTimeout: DispatchWallTime.distantFuture)
+        if delay == DispatchTimeoutResult.success {
+            print("success")
+            
+        }
     }
     
 }
