1b9de0b19d791e02095e025404c258e5c82327ad
mfang032 <flcyfr@163.com>
2017-07-24 09:24:54 +0800

    Subject demo

---  函数式编程和响应式编程/RxSwift-Observable/RxSwift-Observable/SubjectDemo.playground/Contents.swift
+++  函数式编程和响应式编程/RxSwift-Observable/RxSwift-Observable/SubjectDemo.playground/Contents.swift
@@ -18,6 +18,30 @@ example(of: "PublishSubject") {
     })
     subject.on(.next("1"))
     subject.onNext("2")
+    
+    let subscriptionTwo = subject
+        .subscribe({ (event) in
+        print("2)", event.element ?? event)
+    })
+    subject.onNext("3")
+    subscriptionOne.dispose()
+    subject.onNext("4")
+    
+    subject.onCompleted()
+    subject.onNext("5")
+    subscriptionTwo.dispose()
+    
+    let disposeBag = DisposeBag()
+    subject
+        .subscribe({
+            print("3)", $0.element ?? $0)
+        })
+        .addDisposableTo(disposeBag)
+    subject.onNext("?")
 }
+
+
+
+
 
 
